910f95cbea048454cc4989ab1e62bbd42a72bd41
RossDAI <ross@Rosss-MacBook-Pro.local>
2026-03-20 18:05:33 +0800

    Watched some cat videos

---
+++ LLM_Chat/models/piper/piper_model.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx \
+     https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx.json
---
+++ LLM_Chat/models/text_model/qwen.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget -c https://huggingface.co/unsloth/Qwen3.5-0.8B-GGUF/resolve/main/Qwen3.5-0.8B-UD-Q4_K_XL.gguf
